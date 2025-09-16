ce5496e9ab37d46020ed916e7e5a6d4f38e309d9
Aleksandr Kisel <51911663+Alex991995@users.noreply.github.com>
2025-09-16 12:47:55 +0300

    fix: improting interfsces

--- src/app/shared/interfaces/index.ts
+++ src/app/shared/interfaces/index.ts
@@ -1,18 +1,1 @@
-export interface IStatistic {
-  symbol: string;
-  priceChange: string;
-  priceChangePercent: string;
-  weightedAvgPrice: string;
-  lastPrice: string;
-  lastQty: string;
-  openPrice: string;
-  highPrice: string;
-  lowPrice: string;
-  volume: string;
-  quoteVolume: string;
-  openTime: number;
-  closeTime: number;
-  firstId: number;
-  lastId: number;
-  count: number;
-}
+export * from './statistic';
