a4f99f4f249519a3fb0245f917938c284d20fa03
Aleksandr Kisel <51911663+Alex991995@users.noreply.github.com>
2025-09-16 11:52:35 +0300

    fix: small correction

--- src/app/features/detail-tables/agg-table-transaction/agg-table-transaction.component.ts
+++ src/app/features/detail-tables/agg-table-transaction/agg-table-transaction.component.ts
@@ -5,47 +5,12 @@ import { IAggTrade } from '@pages/detail-crypto-pars/model';
 @Component({
   selector: 'app-agg-table-transaction',
   imports: [DatePipe],
-  template: `<div>
-    <h4>Recent aggregated transactions</h4>
-    <table class="table-sm">
-      <thead>
-        <tr>
-          <th>Time</th>
-          <th>Price</th>
-          <th>Quantity</th>
-          <th>Side</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          [class.text-success]="!aggData()?.m"
-          [class.text-danger]="aggData()?.m"
-        >
-          <td class="placeholder-glow">
-            <span [class]="showPlaceholder(!aggData())"></span>
-            {{ aggData()?.T | date : 'HH:mm:ss' }}
-          </td>
-
-          <td class="placeholder-glow">
-            <span [class]="showPlaceholder(!aggData())"></span>
-            {{ aggData()?.p }}
-          </td>
-          <td class="placeholder-glow">
-            <span [class]="showPlaceholder(!aggData())"></span>
-            {{ aggData()?.q }}
-          </td>
-          <td>
-            {{ aggData()?.m ? 'Sell' : 'Buy' }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>`,
+  templateUrl: './agg-table-transaction.component.html',
 })
 export class AggTableTransactionComponent {
   public aggData = input<IAggTrade | undefined>(undefined);
 
-  showPlaceholder(loadingData: boolean) {
+  skeleton(loadingData: boolean) {
     if (loadingData) {
       return 'placeholder col-12 bg-secondary';
     }
